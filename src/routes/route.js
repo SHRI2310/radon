706bbe04248fec57e7ba19e3a62840ad01ca2a48
SHRI2310 <shrikantajmire1298@gmail.com>
2022-06-03 01:42:29 +0530

    1june1

--- src/routes/route.js
+++ src/routes/route.js
@@ -23,7 +23,23 @@ router.get('/hello', function (req, res) {
     
     
     let arr1 = lodash.chunk(["january","february","march","april","may","june","july","august","september","octomber","november","december"] ,2)
-    console.log(arr1)
+    console.log("here i applied function chunk      "+arr1)
+
+
+let num =[1,3,5,7,9,11,13,15,17,19]
+let tail= lodash.tail(num)
+console.log( "here i applied function tail   "+tail)
+
+let duplicatenum =[1,5,3,5,6,7,2,1,4,1,3,6,7]
+let unionnum = lodash.union(duplicatenum)
+console.log("here i applied function union    "+unionnum)
+
+
+let movies = [["horror","theshining"],["drama","titanic"],["thrilller","shutter island"],["fantasy","pans labyrinth" ]]
+ let frompairs = lodash.fromPairs(movies)
+ console.log("here i applied function frompairs  "+JSON.stringify(fromparis))
+
+     
     res.send('my array !')
 });
 
